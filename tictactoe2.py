4f2b79f37008b191c23e145b20f0d6f2c0587ec9
K Nagesh Nayak <138570737+K-Nagesh-Nayak@users.noreply.github.com>
2023-07-08 00:46:50 +0530

    Update tictactoe2.py

--- tictactoe2.py
+++ tictactoe2.py
@@ -1,5 +1,6 @@
 from tkinter import  *
 from tkinter import messagebox
+
 click=True
 count=0
 
@@ -25,7 +26,7 @@ def check_winner():
             if button[row][col]['text'] !=" ":
                 empty-=1
     if empty==0:
-        messagebox.showerror("TIE")
+        messagebox.showerror("MATCH RESULT","DRAW")
 
 
 def clicked(row,col):
@@ -59,30 +60,8 @@ if __name__=="__main__":
             [0,0,0],
             [0,0,0]]
 
-    '''b1=Button(root,text=' ',bg="white",fg="red",width=20,height=10,command=lambda:clicked(b1))
-    b1.grid(row=0,column=1)
-    b2=Button(root,text=' ',bg="white",fg="red",width=20,height=10,command=lambda:clicked(b2))
-    b2.grid(row=0,column=2)
-    b3=Button(root,text=' ',bg="white",fg="red",width=20,height=10,command=lambda:clicked(b3))
-    b3.grid(row=0,column=3)
-
-    b4=Button(root,text=' ',bg="white",fg="red",width=20,height=10,command=lambda:clicked(b4))
-    b4.grid(row=1,column=1)
-    b5=Button(root,text=' ',bg="white",fg="red",width=20,height=10,command=lambda:clicked(b5))
-    b5.grid(row=1,column=2)
-    b6=Button(root,text=' ',bg="white",fg="red",width=20,height=10,command=lambda:clicked(b6))
-    b6.grid(row=1,column=3)
-
-    b7=Button(root,text=' ',bg="white",fg="red",width=20,height=10,command=lambda:clicked(b7))
-    b7.grid(row=2,column=1)
-    b8=Button(root,text=' ',bg="white",fg="red",width=20,height=10,command=lambda:clicked(b8))
-    b8.grid(row=2,column=2)
-    b9=Button(root,text=' ',bg="white",fg="red",width=20,height=10,command=lambda:clicked(b9))
-    b9.grid(row=2,column=3)'''
-
     frame=Frame(root)
     frame.pack()
-    i=0
     for row in range(3):
         for col in range(3):
             button[row][col]=Button(frame,text=' ',bg="white",fg="red",width=20,height=10,command=lambda row=row,col=col :clicked(row,col))
